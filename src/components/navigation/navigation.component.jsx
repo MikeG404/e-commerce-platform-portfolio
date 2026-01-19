38e324c2f8114b19aa011595f15f542f206aa135
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-01-19 08:49:03 +0100

    Add cart icon and add position styles

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { Fragment } from "react";
 import { Link, Outlet } from "react-router";
 import { useSelector } from 'react-redux';
 import { FaShop } from "react-icons/fa6";
+import { FiShoppingCart } from "react-icons/fi";
 
 import { googleSignInWithPopup, logOut } from '../../utils/firebase/firebase.util';
 
@@ -16,16 +17,21 @@ const Navigation = () => {
             <div className="navigation-container">
                 <div className='logo-container'>
                     <Link to="/">
-                        <FaShop size="50" color="black"/>
+                        <FaShop size="30" color="black"/>
                     </Link>
                 </div>
-                <div className="links-container">
-                    <Link className='link' to='/shop'>SHOP</Link>
-                    { currentUser 
-                        ? <span onClick={() => logOut()}>SIGN OUT</span>
-                        : <span onClick={() => googleSignInWithPopup()}>SIGN IN</span>
-                    }
+                <div className='right-navigation-container'>
+                    <div className="links-container">
+                        <Link className='link' to='/shop'>SHOP</Link>
+                        { currentUser 
+                            ? <span onClick={() => logOut()}>SIGN OUT</span>
+                            : <span onClick={() => googleSignInWithPopup()}>SIGN IN</span>
+                        }
                     </div>
+                    <div className='cart-container'>
+                        <FiShoppingCart size={20} color="black" />
+                    </div>
+                </div>
             </div>
             <Outlet />
         </Fragment>
