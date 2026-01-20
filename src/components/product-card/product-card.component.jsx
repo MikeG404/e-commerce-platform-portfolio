9ba0f2d24d998f0cd72d0bf4d5c54c3fa4558577
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-01-20 09:24:51 +0100

    add price and adapt component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,6 +1,6 @@
 
 const ProductCard = ({product}) => {
-    const { name, imageURL } = product;
+    const { name, imageURL, price } = product;
 
     return (
         <div className="product-card-container">
@@ -8,7 +8,7 @@ const ProductCard = ({product}) => {
             <div className="product-card-footer">
                 <div className="product-infos">
                     <span className="title">{name}</span>
-                    <span className="price">$30</span>
+                    <span className="price">{price}</span>
                 </div>
                 <button>Add To Cart</button>
             </div>
--- src/store/categories-slice/categories-slice.js
+++ src/store/categories-slice/categories-slice.js
@@ -12,17 +12,21 @@ const categoriesSlice = createSlice({
                 {
                     id: 1,
                     name: 'Blue Skinny Jeans',
-                    imageUrl: 'https://images.unsplash.com/photo-1542272604-787c62d465d1?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1542272604-787c62d465d1?w=400',
+                    price: 30
                 },
                 {
                     id: 2,
                     name: 'Black Ripped Jeans',
-                    imageUrl: 'https://images.unsplash.com/photo-1541099810657-40d6a9270acb?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1541099810657-40d6a9270acb?w=400',
+                    price: 30
+
                 },
                 {
                     id: 3,
                     name: 'Light Wash Straight Jeans',
-                    imageUrl: 'https://images.unsplash.com/photo-1505503693641-c55fb0270d71?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1505503693641-c55fb0270d71?w=400',
+                    price: 30
                 }
             ]
         },
@@ -34,17 +38,20 @@ const categoriesSlice = createSlice({
                 {
                     id: 1,
                     name: 'Cotton T-Shirt',
-                    imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=400',
+                    price: 30
                 },
                 {
                     id: 2,
                     name: 'Casual Sweater',
-                    imageUrl: 'https://images.unsplash.com/photo-1556821552-7f41c5d440db?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1556821552-7f41c5d440db?w=400',
+                    price: 60,
                 },
                 {
                     id: 3,
                     name: 'Dress Shirt',
-                    imageUrl: 'https://images.unsplash.com/photo-1598033129519-cb4b50d1b3ff?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1598033129519-cb4b50d1b3ff?w=400',
+                    price: 15,
                 }
             ]
         },
@@ -56,17 +63,20 @@ const categoriesSlice = createSlice({
                 {
                     id: 1,
                     name: 'Programming Guide',
-                    imageUrl: 'https://images.unsplash.com/photo-150784272343-583f20270319?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-150784272343-583f20270319?w=400',
+                    price: 45
                 },
                 {
                     id: 2,
                     name: 'Adventure Novel',
-                    imageUrl: 'https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?w=400',
+                    price: 78
                 },
                 {
                     id: 3,
                     name: 'Self-Help Book',
-                    imageUrl: 'https://images.unsplash.com/photo-1476014422502-898657433e0e?w=400'
+                    imageUrl: 'https://images.unsplash.com/photo-1476014422502-898657433e0e?w=400',
+                    price: 35
                 }
             ]
         }
