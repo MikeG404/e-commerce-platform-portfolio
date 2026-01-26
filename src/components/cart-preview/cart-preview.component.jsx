b1f9d9ed18535da9e5a2ef140663e1b81573722d
Mike Godfrinne <mike.godfrinne@gmail.com>
2026-01-26 19:49:12 +0100

    enhance shopping cart logic

--- src/components/cart-preview/cart-preview.component.jsx
+++ src/components/cart-preview/cart-preview.component.jsx
@@ -1,19 +1,29 @@
 import './cart-preview.styles.scss';
+import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
 import CartItem from '../cart-item/cart-item.component';
+import Button from '../button/button.component';
 
 const CartPreview = () => {
     const cartItems = useSelector(state => state.cart.cartItems);
+    console.log(cartItems.length);
 
     return (
         <div className="cart-preview-container">
-            <h2>Shopping Cart</h2>
-            <ul>
-                {cartItems.map((item) => (
-                    <CartItem key={item.id} item={item} />
-                ))}
-            </ul>
+            <h2 className='title'>Shopping Cart</h2>
+            { cartItems.length !== 0 ?
+                <Fragment>
+                    <ul>
+                        {cartItems.map((item) => (
+                            <CartItem key={item.id} item={item} />
+                        ))}
+                    </ul>
+                    <Button>Checkout</Button>
+                </Fragment>
+                : 
+                <span>No items in cart</span>
+            }
         </div>
     )
 }
